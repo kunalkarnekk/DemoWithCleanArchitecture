4206887410d9df4370d4b349fb9dcfe096f35965
shrik <shrik@SHRIKANTHOME-PC>
2024-08-04 14:40:33 +0530

    Add project files.

---
+++ Application/DTOs/Request/Account/LoginDTOs.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Application.DTOs.Request.Account
+{
+    public class LoginDTOs
+    {
+        [EmailAddress, Required]
+        [RegularExpression("[^@ \\t\\r\\n]+@[^@ \\t\\r\\n]+\\.[^@ \\t\\r\\n]+",
+            ErrorMessage ="Your Email is not valid, provide valid email such as ..")]
+        [Display(Name ="Email Address")]
+        public string  EmailAddress { get; set; } = string.Empty;
+        [Required]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$",
+            ErrorMessage ="Your Password must be mix of Alphanumeric and special characters" )]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,56 @@
+﻿
+
+using Domain.Entity.Authentication;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppDbContext>(o => o.UseSqlServer(config.GetConnectionString("Default")));
+            services.AddIdentityCore<ApplicationUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddSignInManager();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = config["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:key"]!))
+                };
+            });
+            services.AddAuthentication();
+            services.AddAuthorization();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("WebUI",
+                    builder => builder
+                    .WithOrigins("https://localhost:7180")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+            return services;
+        }
+    }
+}
